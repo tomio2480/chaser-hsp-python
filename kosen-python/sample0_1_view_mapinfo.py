ec8270114fbc823ef48c9898c772d8aaeba70b81
tomio2480 <tomio2480@gmail.com>
2021-07-26 13:04:48 +0900

    sample0_1_view_mapinfo.py に周辺情報の表示について HSP とは異なる方法もあるという注釈を追記した．

--- kosen-python/sample0_1_view_mapinfo.py
+++ kosen-python/sample0_1_view_mapinfo.py
@@ -14,5 +14,14 @@ def main():
         # ここまで
         value = client.walk_up()
 
+"""
+【参考】
+今回は HSP 版に合わせた書き方をしていますが，
+Python の場合，10 行目から 13 行目を，
+次の一行に置きかえても周辺情報を見ることができます．
+
+print(value)
+"""
+
 if __name__ == "__main__":
     main()
